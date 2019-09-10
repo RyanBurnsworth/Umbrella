8ae3c9d0265696e7b1c8508e0a0320fca510514e
Ryan Burnsworth <ryanburnsworth@gmail.com>
2019-09-10 15:16:29 -0400

    Initial commit

---
+++ app/src/main/java/com/ryanburnsworth/umbrella/UmbrellaApplication.kt
@@ -0,0 +1,7 @@
+package com.ryanburnsworth.umbrella
+
+import android.app.Application
+
+class UmbrellaApplication: Application() {
+
+}
---
+++ app/src/main/java/com/ryanburnsworth/umbrella/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ryanburnsworth.umbrella.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ryanburnsworth.umbrella.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
